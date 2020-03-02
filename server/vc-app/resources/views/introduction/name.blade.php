3d31b1c264f407207d360e2fb492fca1fb0eeca4
Shinji-MCZ <yoshinoya1234@gmail.com>
2020-03-02 14:14:05 +0900

    課題終了

---
+++ server/vc-app/resources/views/introduction/name.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>name表示</title>
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+  <h1 class="ao">{{$name}}</h1>
+</body>
+</html>
